aadde6ee0a517bc8add21e879b68f96cc3905727
Megan Ong <83198134+meganong1@users.noreply.github.com>
2023-12-06 17:58:42 -0800

    Delete directory

--- PlaylistAppProject/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// Represents the current state of the object in JSON format
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
